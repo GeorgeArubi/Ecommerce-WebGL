5c7f307c10cb6bf6bbfde9e30ba8dc9cd6296ead
DOGHOUSETV <arubigeorge0@gmail.com>
2021-06-10 07:17:57 -0400

    Update webpack.config.development.js

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,1 +1,15 @@
 const { merge } = require('webpack-merge')
+const path = require('path')
+
+const config = require('./webpack.config')
+
+module.exports = merge(config, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        writeToDisk: True
+    },
+    output: {
+        path: path.resolve(__dirname, 'public')
+    }
+})
