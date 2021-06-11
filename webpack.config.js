bae2871faf8afc59c4a00e0c3bfe4fc789d312a9
DOGHOUSETV <arubigeorge0@gmail.com>
2021-06-11 03:38:42 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 
 const CopyWebackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
@@ -39,7 +40,16 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css'
-        })
+        }),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+              plugins: [
+                ["gifsicle", { interlaced: true }],
+                ["jpegtran", { progressive: true }],
+                ["optipng", { optimizationLevel: 5 }],
+              ],
+            },
+          })
     ],
     module: {
         rules: [
@@ -74,10 +84,24 @@ module.exports = {
                 test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
                 loader: 'file-loader',
                 options: {
-                    // name (file) {
-                    //     return '[hash].[ext]'
-                    // }
+                    name (file) {
+                        return '[hash].[ext]'
+                    }
                 }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|webp)$/i,
+                use: [
+                    {
+                        loader: ImageMinimizerPlugin.loader,
+                        options: {
+                            severityError: "warning", // Ignore errors on corrupted images
+                            minimizerOptions: {
+                                plugins: ["gifsicle"],
+                            },
+                        },
+                    },
+                ],
             }
         ]
     }
