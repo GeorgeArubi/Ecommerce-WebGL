c704632c80c889dbd14922ff2a5db44f9fdf5221
DOGHOUSETV <arubigeorge0@gmail.com>
2021-06-11 02:05:41 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
-const dirAssets = path.join(__dirname, 'assets')
+const dirShared = path.join(__dirname, 'assets')
 const dirStyles = path.join(__dirname, 'styles')
 const dirNode = 'node_modules'
 
@@ -19,9 +19,26 @@ module.exports = {
     resolve: {
         modules: [
             dirApp,
-            dirAssets,
+            dirShared,
             dirStyles,
             dirNode 
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEVELOPMENT
+        }),
+        new CopyWebackPlugin({
+            patterns:[
+                {
+                    from: './shared',
+                    to: ''
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
 }
