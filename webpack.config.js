bf04bedcb0146f60a332b31001edd5489f9f3336
DOGHOUSETV <arubigeorge0@gmail.com>
2021-06-11 03:03:25 -0400

    boilerplate setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,10 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
-const dirShared = path.join(__dirname, 'assets')
+const dirImages = path.join(__dirname, 'images')
+const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
+const dirVideos = path.join(__dirname, 'videos')
 const dirNode = 'node_modules'
 
 module.exports = {
@@ -19,8 +21,10 @@ module.exports = {
     resolve: {
         modules: [
             dirApp,
+            dirImages,
             dirShared,
             dirStyles,
+            dirVideos,
             dirNode 
         ]
     },
@@ -40,5 +44,45 @@ module.exports = {
             filename: '[name].css',
             chunkFilename: '[id].css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+                loader: 'file-loader',
+                options: {
+                    // name (file) {
+                    //     return '[hash].[ext]'
+                    // }
+                }
+            }
+        ]
+    }
 }
